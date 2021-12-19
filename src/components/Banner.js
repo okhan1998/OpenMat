3707ac5f032d319f84246322e0b3baf48b111ae1
okhan1998 <whwlgns111@gmail.com>
2021-12-19 22:06:07 +0900

    Add Header

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import 
+function Banner() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Home component</h1>
+            <Banner />
+        </div>
+    )
+}
+
+export default Home
