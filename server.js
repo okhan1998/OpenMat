97af2ea06a18ae69a64dd4d07eb3495a9d86f27d
okhan1998 <whwlgns111@gmail.com>
2021-12-25 01:42:33 +0900

    create database connection

--- server.js
+++ server.js
@@ -5,21 +5,30 @@ const port = process.env.PORT || 5001;
 
 app.use(express.static('build'));
 
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/build/index.html')
 })
 
 app.get('/openmat/api', (req, res) => {
-    res.send([
-        {
-            'img': 'https://images.squarespace-cdn.com/content/v1/53406330e4b0c7c09a3b7926/1575790977666-0ZBOSITFCUHW2XAP1JLR/new+mgd+2019+-+modded.jpg?format=1500w',
-            'location': '경기 김포시 사우중로73번길 24 선일프라자',
-            'title': '김포 골든라이언 주말 오픈맷',
-            'description': '12월 4일 오후 5시 ~ 오후 7시. 화이트 2그랄 이상부터 참여가능',
-            'star': '4.73',
-            'price': '30,000원'
+    connection.query(
+        'SELECT * FROM gym',
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ])   
+    );
 })
 
 
