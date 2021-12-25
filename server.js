6f9e7f42a9a9ed9f7381ba7e3df5b34d699a62e7
okhan1998 <whwlgns111@gmail.com>
2021-12-25 15:43:22 +0900

    set configvar

--- server.js
+++ server.js
@@ -2,21 +2,31 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
-
+let connection = null;
 app.use(express.static('build'));
-
-const data = fs.readFileSync('./database.json')
-const conf = JSON.parse(data);
 const mysql = require('mysql');
+if(port != 5001){
+    connection = mysql.createConnection({
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        port: process.env.password,
+        database: process.env.database
+    });
+    connection.connect();
+}else {
+    const data = fs.readFileSync('./database.json')
+    const conf = JSON.parse(data);
+    connection = mysql.createConnection({
+        host: conf.host,
+        user: conf.user,
+        password: conf.password,
+        port: conf.port,
+        database: conf.database
+    });
+    connection.connect();
+}
 
-const connection = mysql.createConnection({
-    host: conf.host,
-    user: conf.user,
-    password: conf.password,
-    port: conf.port,
-    database: conf.database
-});
-connection.connect();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/build/index.html')
